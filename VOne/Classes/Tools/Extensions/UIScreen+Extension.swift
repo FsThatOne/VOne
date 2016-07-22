a20bf86254dd013eaba7fa3121b60978d5db5dc1
王正一 <fsymbio@icloud.com>
2016-07-22 17:15:54 +0800

    自定义tabbar

---
+++ VOne/Classes/Tools/Extensions/UIScreen+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Screen+Extension.swift
+//  VOne
+//
+//  Created by 王正一 on 16/7/22.
+//  Copyright © 2016年 FsThatOne. All rights reserved.
+//
+
+import Foundation
---
+++ VOne/Classes/View/Contact/ContactViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ContactViewController.swift
+//  VOne
+//
+//  Created by 王正一 on 16/7/21.
+//  Copyright © 2016年 FsThatOne. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: FSBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        // Do any additional setup after loading the view.
+    }
+
+    override func setupUI() {
+        view.backgroundColor = UIColor.yellow()
+    }
+}
+
+extension ContactViewController {
+    
+}
